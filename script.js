5f00892f7813752d1bd45b58bab47b143c5d15a1
Mark <MarkKranenburg@hotmail.nl>
2023-11-03 19:11:18 +0100

    add SVG for animation

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const displayThemeSpan = document.querySelector('#displayThemeSpan');
 const body = document.querySelector('body');
 const root = document.documentElement;
 
+const SVGsun = document.querySelector('#SVG-Sun');
+const SVGmoon = document.querySelector('#SVG-Moon');
+
 let isDarkmodeOn = false;
 
 function toggleTheme() {
   toggleSpan(displayThemeSpan.textContent);
   root.className === 'lightTheme' ? root.className = 'darkTheme': root.className = 'lightTheme';
+  isDarkmodeOn === false ? SVGsun.classList.add('SVG-Sun-ThemeDark') : SVGsun.classList.remove('SVG-Sun-ThemeDark');
+  isDarkmodeOn === false ? SVGmoon.classList.add('SVG-Moon-ThemeDark') : SVGmoon.classList.remove('SVG-Moon-ThemeDark');
   isDarkmodeOn = !isDarkmodeOn;
 }
 
