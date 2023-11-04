e95b445fed0bdd571f51fedd0036fa03f8313d07
Mark <MarkKranenburg@hotmail.nl>
2023-11-04 13:32:25 +0100

    make animation sun and moon toggle

--- script.js
+++ script.js
@@ -4,18 +4,50 @@ const root = document.documentElement;
 
 const SVGsun = document.querySelector('#SVG-Sun');
 const SVGmoon = document.querySelector('#SVG-Moon');
+const SVGstar = document.querySelector('#SVG-Star');
+
+const sunParent = document.querySelector('#sunParent');
+const sunChildren = Array.from(sunParent.children);
 
 let isDarkmodeOn = false;
 
 function toggleTheme() {
   toggleSpan(displayThemeSpan.textContent);
   root.className === 'lightTheme' ? root.className = 'darkTheme': root.className = 'lightTheme';
-  isDarkmodeOn === false ? SVGsun.classList.add('SVG-Sun-ThemeDark') : SVGsun.classList.remove('SVG-Sun-ThemeDark');
-  isDarkmodeOn === false ? SVGmoon.classList.add('SVG-Moon-ThemeDark') : SVGmoon.classList.remove('SVG-Moon-ThemeDark');
+  isDarkmodeOn === false ? giveSunDarkTheme() : giveSunLightTheme();
+  isDarkmodeOn === false ? giveMoonDarkTheme() : giveMoonLightTheme();
   isDarkmodeOn = !isDarkmodeOn;
 }
 
 function toggleSpan(theme) {
   isDarkmodeOn === false ? theme = 'Dark' : theme = 'Light';
   displayThemeSpan.textContent = theme;
+}
+
+function giveMoonDarkTheme() {
+  SVGmoon.classList.remove('moonLight');
+  SVGmoon.classList.add('moonDark');
+  SVGstar.classList.remove('moonLight');
+  SVGstar.classList.add('moonDark');
+}
+
+function giveMoonLightTheme() {
+  SVGmoon.classList.remove('moonDark');
+  SVGmoon.classList.add('moonLight');
+  SVGstar.classList.remove('moonDark');
+  SVGstar.classList.add('moonLight');
+}
+
+function giveSunDarkTheme() {
+  sunChildren.forEach((child) => {
+    child.classList.remove('sunLight');
+    child.classList.add('sunDark');
+  });
+}
+
+function giveSunLightTheme() {
+  sunChildren.forEach((child) => {
+    child.classList.remove('sunDark');
+    child.classList.add('sunLight');
+  });
 }
